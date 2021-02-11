f51957bc9b4822c19b1fd99fcfdac4c1610e8a53
David Hansson <hansson.d.david@gmail.com>
2021-02-11 19:25:47 +0100

    Full on working func

--- functions/index.js
+++ functions/index.js
@@ -7,67 +7,63 @@ const db = admin.firestore();
 const puppeteer = require('puppeteer');
 const chromium = require('chrome-aws-lambda');
 
-exports.writetest = functions
-    .https.onRequest(async (req, res) => {
-        let num = req.query.num ?? 0
-        await db.collection('articles').add({ title: "title" + num, link: "link" + num, img: "img" + num, provider: "euroinvestor", date: admin.firestore.Timestamp.now() });
-        res.json({ result: `idk` });
-    })
-
-exports.gettest = functions
-    .https.onRequest(async (req, res) => {
-        const articles = db.collection("articles");
-        const snapshot = await articles.where("provider", "==", "euroinvestor").orderBy("date").limit(40).get();
-
-        let items = []
-        snapshot.forEach(doc => items.push(doc.data()));
-        res.json({ result: items });
-    })
-
 exports.newsscraper = functions
     .region("europe-west1")
     .runWith({ timeoutSeconds: 540, memory: "4GB" })
     .https.onRequest(async (req, res) => {
-        try {
-            let start = new Date().getTime();
-            const browser = await puppeteer.launch({
-                args: [
-                    '--disable-gpu',
-                    '--disable-dev-shm-usage',
-                    '--disable-setuid-sandbox',
-                    '--no-first-run',
-                    '--no-sandbox',
-                    '--no-zygote',
-                    '--single-process'
-                ],
-                defaultViewport: chromium.defaultViewport,
-                headless: chromium.headless
-            });
-            const page = await browser.newPage();
-            await page.goto("https://www.euroinvestor.dk/nyheder");
-            await page.waitForSelector(".content-wrapper");
 
-            var items = await page.evaluate(() => {
-                let data = [];
-                let articles = document.getElementsByTagName("article");
+        let start = new Date().getTime();
+        const browser = await puppeteer.launch({
+            args: [
+                '--disable-gpu',
+                '--disable-dev-shm-usage',
+                '--disable-setuid-sandbox',
+                '--no-first-run',
+                '--no-sandbox',
+                '--no-zygote',
+                '--single-process'
+            ],
+            defaultViewport: chromium.defaultViewport,
+            headless: chromium.headless
+        });
+        const page = await browser.newPage();
+        await page.goto("https://www.euroinvestor.dk/nyheder");
+        await page.waitForSelector(".content-wrapper");
 
-                for (let i = 0; i < articles.length; i++) {
-                    let title = articles[i].querySelector("div > a:last-child > h2").innerHTML;
-                    let link = articles[i].querySelector("div a:last-child").href;
-                    let img = articles[i].childNodes.length == 2 ? articles[i].querySelector("a").href : "";
+        var items = await page.evaluate(() => {
+            let data = []; let articles = document.getElementsByTagName("article");
 
-                    data.push({ title: title, link: link, img: img });
-                }
+            articles.forEach(article => {
+                let title = article.querySelector("div > a:last-child > h2").innerHTML;
+                let link = article.querySelector("div a:last-child").href;
+                let img = article.childNodes.length == 2 ? article.querySelector("figure").getAttribute("data-original") : "";
 
-                return data;
+                data.push({ title: title, link: link, img: img, provider: "euroinvestor" });
             });
 
-            await browser.close();
-            var end = new Date().getTime();
-            items.unshift({ time: end - start })
-            return res.json(items);
+            return data;
+        });
 
-        } catch (err) {
-            return res.json({ result: "Error" });
+        // Operation done close browser
+        await browser.close();
+
+        // Get 50 latest articles from Firestore
+        const articlesCollection = db.collection("articles");
+        const snapshot = await articlesCollection.where("provider", "==", "euroinvestor").orderBy("date").limit(50).get();
+        let articles = [];
+        snapshot.forEach(doc => articles.push(doc.data()));
+
+        // Remove duplicates
+        let final = items.filter(item => !articles.find(article => item.title === article.title));
+
+        // Add ms time
+        var ms = (new Date().getTime()) - start;
+        final.map(item => item.time = ms);
+
+        // Add to Firebase Firestore collection
+        for (let i = 0; i < final.length; i++) {
+            await db.collection("articles").add({ ...final[i], ...{ date: admin.firestore.Timestamp.now() } });
         }
+
+        return res.json({ result: "Success" });
     });
